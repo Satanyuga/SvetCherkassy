884e5f5abdedba9fdf12462d47701e7257db1dd4
Satanyuga <37559534+Satanyuga@users.noreply.github.com>
2026-02-11 14:42:46 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,14 @@
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('svet-store').then((cache) => cache.addAll([
-      '/',
-      '/index.html',
-      '/data.json'
-    ])),
-  );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
-  );
+self.addEventListener('push', (event) => {
+  const data = event.data ? event.data.json() : { title: 'Внимание!', body: 'График изменился' };
+  const options = {
+    body: data.body,
+    icon: 'https://cdn-icons-png.flaticon.com/512/2983/2983973.png',
+    badge: 'https://cdn-icons-png.flaticon.com/512/2983/2983973.png',
+    vibrate: [200, 100, 200]
+  };
+  event.waitUntil(self.registration.showNotification(data.title, options));
 });
